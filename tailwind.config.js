c3c854394bff6cf4711ed09364b829ff2dae188c
Danilo Mello <daniloomelloo@gmail.com>
2023-03-07 15:04:24 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      colors: {
+        'dudu': {
+          200: '#191919',
+          400: '#121212',
+        }
+      }
+    },
+
+  },
+  plugins: [],
+}
